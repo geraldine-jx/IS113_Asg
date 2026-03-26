42a6194f0b4dc7865f8436dcc10b19d67dfd15dd
geraldine-jx <geraldine.k.2025@computing.smu.edu.sg>
2026-03-26 14:55:50 +0800

    CRUD fav

---
+++ routes/homeRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const homePageController = require("../controllers/homePageController");
+
+// GET homepage (full URL: /home-display)
+router.get("/", homePageController.getAllPets);
+
+// Toggle favourite (full URL: /home-display/pets/:id/toggleFavourite)
+router.post("/pets/:id/toggleFavourite", homePageController.toggleFavourite);
+
+// Get more info for a pet (full URL: /home-display/pets/:id)
+router.get("/pets/:id",homePageController.getPetDetails)
+
+module.exports = router;
