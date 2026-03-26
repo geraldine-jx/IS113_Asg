31b1f4d92d8a34e46d0bffeb0672ece58d034542
valenciatty-oss <valenciatty@gmail.com>
2026-03-26 16:30:34 +0800

    appt

--- server.js
+++ server.js
@@ -1,5 +1,17 @@
 // Main area to facilitate the Pets platform
 const express = require("express");
+<<<<<<< Updated upstream
+=======
+const path = require("path");
+const mongoose = require("mongoose");
+const session = require("express-session");
+
+const authRoutes = require("./routes/authRoutes");
+const homeRoutes = require("./routes/homeRoutes");
+const formRoutes = require("./routes/formPage");
+const appointmentRoutes = require("./routes/appointmentRoutes");
+
+>>>>>>> Stashed changes
 const server = express();
 const path = require("path");
 
@@ -20,6 +32,35 @@ server.use("/home-display", home);
 const hostname = "127.0.0.1";
 const port = 8000;
 
+<<<<<<< Updated upstream
+=======
+// View engine
+server.set("view engine", "ejs");
+
+// Middleware
+server.use("/", express.static(path.join(__dirname, "public")));
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json()); // important for AJAX requests
+
+server.use(session({
+  secret: "mypetappsecret",
+  resave: false,
+  saveUninitialized: false
+}));
+
+// MongoDB connection
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Routes
+server.use("/", authRoutes);
+server.use("/", formRoutes);
+server.use("/home-display", homeRoutes);
+server.use("/", appointmentRoutes);
+
+// Start server
+>>>>>>> Stashed changes
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
