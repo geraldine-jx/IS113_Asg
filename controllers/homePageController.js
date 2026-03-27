54923fb4a070bdbd745d70af175380c72375aa5e
geraldine-jx <geraldine.k.2025@computing.smu.edu.sg>
2026-03-27 16:13:11 +0800

    Update feature (viewCount and rec)

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -2,32 +2,70 @@ const Pet = require("../models/pet.js");
 const Favourite = require("../models/favourite");
 const User = require("../models/user");
 
+// Get user favourite
+async function getUserFavourites(userId) {
+  return await Favourite.find({ user: userId });
+}
+
+// Toggle favourite status
+async function toggleFavouriteStatus(userId, petId) {
+  const favourite = await Favourite.findOne({ user: userId, pet: petId });
+  if (favourite) {
+    await Favourite.findByIdAndDelete(favourite._id);
+    return false; 
+  } else {
+    await Favourite.create({ user: userId, pet: petId });
+    return true;
+  }
+}
+
+// Increase view count for pet in favourite
+async function incrementViewCount(userId, petId) {
+  const fav = await Favourite.findOneAndUpdate(
+    { user: userId, pet: petId },
+    { $inc: { viewCount: 1 } },
+    { new: true }
+  );
+  return fav;
+}
+
+//For reccomendation,get pet with top view count
+async function getTopFavouritePet(favDocs) {
+  if (!favDocs || favDocs.length === 0) return null;
+
+  let topFav = favDocs[0];
+  for (let i = 1; i < favDocs.length; i++) {
+    if (favDocs[i].viewCount > topFav.viewCount) {
+      topFav = favDocs[i];
+    }
+  }
+  return await Pet.findById(topFav.pet);
+}
+
+//homepage
 exports.getAllPets = async (req, res) => {
   try {
-    
-    let favouritePetIds = [];
-    let userId = req.session.userId; 
+    //make sure user is logged in
+    const userId = req.session.userId;
+    if (!userId) return res.redirect("/");
 
-    if (userId) {
-      // Get favourites for logged-in user
-      const favourites = await Favourite.find({ user: userId });
-      favouritePetIds = favourites.map(f => f.pet.toString());
-    }
-
-    // Get all pets
+    const favourites = await getUserFavourites(userId);
+    const favouritePetIds = favourites.map(f => f.pet.toString());
     const pets = await Pet.find();
 
-    // If user has favourites, put favourite first that remaining pets
     const sortedPets = favouritePetIds.length
       ? [
           ...pets.filter(p => favouritePetIds.includes(p._id.toString())), // favourites first
           ...pets.filter(p => !favouritePetIds.includes(p._id.toString())) // remaining pets
         ]
       : pets; // no favourites → just all pets
+    
+    const recommendedPet = await getTopFavouritePet(favourites);
 
     res.render("pet/home-display", {
       pets: sortedPets,
-      favourites: favouritePetIds
+      favourites: favouritePetIds,
+      recommendedPet
     });
 
   } catch (err) {
@@ -39,19 +77,9 @@ exports.getAllPets = async (req, res) => {
 // toggle for favourite pet
 exports.toggleFavourite = async (req, res) => {
   try {
-    if (!req.session.userId) return res.redirect("/login");
-
     const userId = req.session.userId;
     const petId = req.params.id;
-
-    const favourite = await Favourite.findOne({ user: userId, pet: petId });
-    // Delete a favourites data
-    if (favourite) {
-      await favourite.deleteOne();
-    } else {
-    // Create a favourites data
-      await Favourite.create({ user: userId, pet: petId });
-    }
+    await toggleFavouriteStatus(userId, petId);
 
     res.redirect("/home-display");
   } catch (err) {
@@ -62,20 +90,37 @@ exports.toggleFavourite = async (req, res) => {
 
 // Get details for more info page
 exports.getPetDetails = async (req, res) => {
-    try {
-        const petId = req.params.id;
-        //find pet
-        const pet = await Pet.findById(petId);
-        const user = await User.findById(req.session.userId);
-        const favourites = user ? user.favourites.map(id => id.toString()) : [];
+  try {
+    const petId = req.params.id;
+    const userId = req.session.userId;
 
-        if (!pet) {
-            return res.send("Pet not found");
+    const pet = await Pet.findById(petId);
+    if (!pet) return res.send("Pet not found");
+
+    let favourites = [];
+    let viewCount = null;
+
+    if (userId) {
+      const favDocs = await getUserFavourites(userId);
+      favourites = favDocs.map(f => f.pet.toString());
+
+      // Check if this pet is a favourite
+      const fav = await incrementViewCount(userId, petId);
+      if (fav) {
+        viewCount = fav.viewCount; 
         }
-        
-        res.render("pet/pet-details", { pet,favourites });
-    } catch (err) {
-        console.error(err);
-        res.send("Error loading pet details");
     }
+
+    res.render("pet/pet-details", {
+      pet,
+      favourites,
+      viewCount
+    });
+
+  } catch (err) {
+    console.error(err);
+    res.send("Error loading pet details");
+  }
 };
+
+
